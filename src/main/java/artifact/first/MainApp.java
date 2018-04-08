42082c3904529e1babf981d2699e621944df27fc
Marco <bolognesi.marco@gmail.com>
2018-04-08 21:33:27 +0200

    prova_hook

--- src/main/java/artifact/first/MainApp.java
+++ src/main/java/artifact/first/MainApp.java
@@ -1,5 +1,19 @@
 package artifact.first;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class MainApp {
-	public static void main(String[] aa ) {}
+	@RequestMapping("/")
+    public String home() {
+        return "Hello Docker World 2";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp.class, args);
+    }
 }
